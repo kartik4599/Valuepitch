8076e1f747ac48806848fa8a48e93b8e3dcc7997
kartik4599 <kartikmendu@gmail.com>
2024-08-31 16:45:36 +0530

    minor change in auth service

--- src/app.tsx
+++ src/app.tsx
@@ -7,16 +7,21 @@ import { RootState } from "./redux/store";
 import { loginUser, logoutUser } from "./redux/user-slice";
 import { Toaster } from "sonner";
 import { addProfile } from "./redux/profile-slice";
+
 const App = () => {
   const user = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch();
 
   const fetchUser = async () => {
-    const token = localStorage.getItem("token");
-    if (!token) return dispatch(logoutUser());
-    const { data, profile } = await getMyData();
-    dispatch(loginUser(data));
-    dispatch(addProfile(profile));
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) return dispatch(logoutUser());
+      const { data, profile } = await getMyData();
+      dispatch(loginUser(data));
+      dispatch(addProfile(profile));
+    } catch (e) {
+      dispatch(logoutUser());
+    }
   };
 
   useEffect(() => {
